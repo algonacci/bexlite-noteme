bd7c78829a14a85eaba48770730a09463da7167e
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-06-26 16:41:22 +0700

    get and create note

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { client } from "./models/client";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/notes", () => {
+    const allNotes = client.query(`SELECT * FROM notes`).all()
+    return {
+      status: {
+        code: 200,
+        message: "Success fetching all notes"
+      },
+      data: allNotes
+    }
+  })
+  .post("/notes", ({ body }) => {
+    const { content } = body
+    const createNote = client.query("INSERT INTO notes (content) VALUES (?)").all(content)
+    return {
+      status: {
+        code: 200,
+        message: "Success fetching all notes"
+      },
+      data: createNote
+    }
+  }, {
+    body: t.Object({
+      content: t.String()
+    })
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
